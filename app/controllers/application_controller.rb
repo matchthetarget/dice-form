8b460565c7006b7d43afdf021eb6ad4d2c5034df
Ben Purinton <ben.purinton@gmail.com>
2023-10-17 15:06:10 -0400

    fixed params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,14 @@ class ApplicationController < ActionController::Base
   end
 
   def dice_roll
-    @num_dice = params.fetch("number_of_dice").to_i
-    @sides = params.fetch("how_many_sides").to_i
+    @number_of_dice = params.fetch("dice").to_i
+    @how_many_sides = params.fetch("sides").to_i
 
     @rolls = []
 
-    @num_dice.times do
-      die = rand(1..@sides)
+    @number_of_dice.times do
+      die = rand(1..@how_many_sides)
+
       @rolls.push(die)
     end
 
--- spec/features/dice_spec.rb
+++ spec/features/dice_spec.rb
@@ -8,109 +8,28 @@ describe "root URL" do
   end
 end
 
-describe "root URL" do
-  it "has links to '/dice/X/Y', where 'X' is the number of dice, and 'Y' is the number of sides", points: 1 do
-    visit "/"
-
-    expect(page).to have_tag("a", :with => { :href => "/dice/2/6" })
-    expect(page).to have_tag("a", :with => { :href => "/dice/2/10" })
-    expect(page).to have_tag("a", :with => { :href => "/dice/1/20" })
-    expect(page).to have_tag("a", :with => { :href => "/dice/5/4" })
-  end
-end
-
-describe "/dice/2/6" do
-  it "has a level one heading with the text '2d6'", points: 1 do
-    visit "/dice/2/6"
-
-    expect(page).to have_tag("h1", text: /2d6/i)
-  end
-end
-
-describe "/dice/2/6" do
-  it "displays the outcome of rolling two six-sided dice in li's of an unordered list", points: 1 do
-    visit "/dice/2/6"
-
-    expect(page).to have_tag("ul") do
-      with_tag("li", :text => /\d+/, :count => 2)
-    end
-  end
-end
-
-describe "/dice/2/10" do
-  it "has a level one heading with the text '2d10'", points: 1 do
-    visit "/dice/2/10"
-
-    expect(page).to have_tag("h1", text: /2d10/i)
-  end
-end
-
-describe "/dice/2/10" do
-  it "displays the outcome of rolling two six-sided dice in li's of an unordered list", points: 1 do
-    visit "/dice/2/10"
-
-    expect(page).to have_tag("ul") do
-      with_tag("li", :text => /\d+/, :count => 2)
-    end
-  end
-end
-
-describe "/dice/1/20" do
-  it "has a level one heading with the text '1d20'", points: 1 do
-    visit "/dice/1/20"
-
-    expect(page).to have_tag("h1", text: /1d20/i)
-  end
-end
-
-describe "/dice/1/20" do
-  it "displays the outcome of rolling one 20-sided dice in li's of an unordered list", points: 1 do
-    visit "/dice/1/20"
-
-    expect(page).to have_tag("ul") do
-      with_tag("li", :text => /\d+/, :count => 1)
-    end
-  end
-end
-
-describe "/dice/5/4" do
-  it "has a level one heading with the text '5d4'", points: 1 do
-    visit "/dice/5/4"
-
-    expect(page).to have_tag("h1", text: /5d4/i)
-  end
-end
-
-describe "/dice/5/4" do
-  it "displays the outcome of rolling one four-sided dice in li's of an unordered list", points: 1 do
-    visit "/dice/5/4"
-
-    expect(page).to have_tag("ul") do
-      with_tag("li", :text => /\d+/, :count => 5)
-    end
-  end
-end
-
-describe "/dice/[RANDOM_DICE]/[RANDOM_SIDES]" do
+describe "/process_roll?dice=[RANDOM_DICE]&sides=[RANDOM_SIDES]" do
   it "has a level one heading with the text '[RANDOM_DICE]d[RANDOM_SIDES]'", points: 1 do
 
     random_dice = rand(100)
     random_sides = rand(100)
-    visit "/dice/#{random_dice}/#{random_sides}"
+
+    visit "/process_roll?dice=#{random_dice}&sides=#{random_sides}"
 
     expect(page).to have_tag("h1", text: "#{random_dice}d#{random_sides}")
   end
 end
 
-describe "/dice/[RANDOM_DICE]/[RANDOM_SIDES]" do
-  it "displays the outcome of rolling [RANDOM_DICE] [RANDOM_SIDES]-sided dice in li's of an unordered list", points: 3 do
+describe "/process_roll?dice=[RANDOM_DICE]&sides=[RANDOM_SIDES]" do
+  it "displays the outcome of rolling two six-sided dice in strong HTML elements in an ordered list", points: 1 do
 
     random_dice = rand(100)
     random_sides = rand(100)
-    visit "/dice/#{random_dice}/#{random_sides}"
 
-    expect(page).to have_tag("ul") do
-      with_tag("li", :text => /\d+/, :count => random_dice)
+    visit "/process_roll?dice=#{random_dice}&sides=#{random_sides}"
+
+    expect(page).to have_tag("ol") do
+      with_tag("strong", :text => /\d+/, :count => random_dice)
     end
   end
 end
